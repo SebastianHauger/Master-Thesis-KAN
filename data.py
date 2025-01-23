55f0e77ec8411df3a23449bc4a6c304cd1a5208e
Sebastian Vogel Hauger <sebose2501@gmail.com>
2025-01-23 10:56:58 +0100

    added training function and plotting function for all fields

--- data.py
+++ data.py
@@ -1,7 +1,4 @@
 """here we add code for loading and alternatively preprocessing data """
-import glob
-import h5py
-from torch.utils.data import DataLoader
 import torch
 import numpy as np
 from the_well.data import WellDataset
@@ -9,27 +6,34 @@ import matplotlib.pyplot as plt
 from einops import rearrange
 
 
-def get_all_data(index = 3) -> torch.Tensor:
-    """a function that later will be for getting all data. At the moment we only use it 
-    to get a single frame."""
+def get_all_data() -> torch.Tensor:
+    """
+    a function that later will be for getting all data. At the moment we only use it 
+    to get a single frame.
+    
+    OUTPUT: a WellDataset which is a type of torch DataSet which is structured as 
+    First we have a list of each input output pair
+    Each instance contains a dictionary with field names where the only relevant 
+    ones for this project are input/output fields 
+    Each of these contain a tensor of shape (n_samples, 256, 512, 3)
+    """
     path_to_repo = "datasets"
     ds = WellDataset(
         well_base_path=path_to_repo,
         well_dataset_name="planetswe",
         well_split_name="test",
-        n_steps_input=1000,
-        n_steps_output=8,
+        n_steps_input=1,
+        n_steps_output=1,
         use_normalization=False
     )
     return ds
 
 
-def plot_an_image_frame(ds):
+def plot_an_image_frame(ds, field_names, tt=1007):
     F = ds.metadata.n_fields
-    x = ds[42]["input_fields"]
+    x = ds[1]["input_fields"]
     x = rearrange(x, "T Lx Ly F -> F T Lx Ly")
-
-    fig, axs = plt.subplots(F, 4, figsize=(4 * 2.4, F * 1.2))
+    fig, axs = plt.subplots(F, 4, figsize=(4 * 2.2, F * 1.2))
 
     for field in range(F):
         vmin = np.nanmin(x[field])
@@ -37,34 +41,29 @@ def plot_an_image_frame(ds):
 
         axs[field, 0].set_ylabel(f"{field_names[field]}")
 
-        for t in range(4):
-            axs[field, t].imshow(
+        for i,t in enumerate([0, int(tt/3), int(2*tt/3), tt]):
+            axs[field, i].imshow(
                 x[field, t], cmap="RdBu_r", interpolation="none", vmin=vmin, vmax=vmax
             )
-            axs[field, t].set_xticks([])
-            axs[field, t].set_yticks([])
+            axs[field, i].set_xticks([])
+            axs[field, i].set_yticks([])
 
-            axs[0, t].set_title(f"$x_{t}$")
+            axs[0, i].set_title(f"$t={t}$")
 
     plt.tight_layout()
+    plt.savefig(f"images/evolution_1year.pdf",bbox_inches="tight", dpi=600)
     plt.show()
     
-    
-
-
-def load_datafile(a):
-    train = DataLoader(a, 2008, False)
-    return train
 
 
 if __name__=='__main__':
-    ds = get_all_data()
+    ds = get_all_data() 
     # dsl = load_datafile(ds)
-    item = ds[0]
+    item = ds[1]
     keyss = list(item.keys()) 
     for key in keyss:
         print(f"key: {key} shape: {item[key].shape}")
     field_names = [name for group in ds.metadata.field_names.values() for name in group]
     print (field_names)
     print(len(ds))
-    plot_an_image_frame(ds)
+    plot_an_image_frame(ds, field_names)
---
+++ main.py
@@ -0,0 +1,31 @@
+"""root of the simulation. Run files from here."""
+from torch.utils.data import DataLoader
+from UKAN import UKAN
+from tqdm import tqdm 
+from data import get_all_data
+from einops import rearrange
+import torch
+
+
+def train_UKAN(epochs, lr):
+    train = get_all_data()
+    train_loader = DataLoader(train, batch_size=64, shuffle=True)
+    model=UKAN()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    
+    for _ in range(epochs):
+        for batch in (bar := tqdm(train_loader)):
+            x= batch["input_fields"]
+            x = rearrange(x, "B Ti Lx Ly F -> B (Ti F) Lx Ly")
+            y = batch["output_fields"]
+            y = rearrange(y, "B To Lx Ly F -> B (To F) Lx Ly")
+            yp = model(x)
+            mse = (yp-y).square().mean()
+            mse.backward()
+            optimizer.zero_grad()
+            bar.set_postfix(loss=mse.detach.item())
+            
+    
+
+if __name__=='__main__':
+    pass
