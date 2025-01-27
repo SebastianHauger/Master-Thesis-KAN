b38f5784c19166bc7ac0c86a17345f0d4152388b
Sebastian Vogel Hauger <sebose2501@gmail.com>
2025-01-27 16:07:59 +0100

    added validation to training

--- main.py
+++ main.py
@@ -13,13 +13,15 @@ def train_UKAN(epochs, lr):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print(device)
     train = get_data("train", PATH_TO_BASE_ALVIS)
+    val = get_data("val", PATH_TO_BASE_ALVIS)
     train_loader = DataLoader(train, batch_size=64, shuffle=True)
+    val_loader = DataLoader(val, batch_size=64, shuffle=False)
     model=UKAN()
     model=model.to(device)
     model.train()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     
-    for _ in range(epochs):
+    for i in range(epochs):
         for batch in (bar := tqdm(train_loader)):
             x = batch["input_fields"]
             x = x.to(device)
@@ -32,6 +34,21 @@ def train_UKAN(epochs, lr):
             mse.backward()
             optimizer.zero_grad()
             bar.set_postfix(loss=mse.detach().item())
+        with torch.no_grad():
+            avg_mse=0
+            for n,batch in enumerate(val_loader):
+                x = batch["input_fields"]
+                x = x.to(device)
+                x = rearrange(x, "B Ti Lx Ly F -> B (Ti F) Lx Ly")
+                y = batch["output_fields"]
+                y = y.to(device)
+                y = rearrange(y, "B To Lx Ly F -> B (To F) Lx Ly")
+                yp = model(x)
+                mse = (yp-y).square().mean()
+                avg_mse = (avg_mse*n + mse)/(n+1) # mooving average MSE
+            print(f"validation loss epoch {i}: {avg_mse}")
+                
+                
         
             
     
