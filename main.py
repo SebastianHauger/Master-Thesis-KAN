2103cd13ff155f1545a084324dd456489ac23934
Sebastian Vogel Hauger <sebose2501@gmail.com>
2025-01-27 11:44:06 +0100

    added cuda option

--- main.py
+++ main.py
@@ -8,23 +8,29 @@ import torch
 
 
 def train_UKAN(epochs, lr):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(device)
     train = get_all_data()
     train_loader = DataLoader(train, batch_size=8, shuffle=True)
     model=UKAN()
+    model=model.to(device)
     model.train()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     
     for _ in range(epochs):
         for batch in (bar := tqdm(train_loader)):
-            x= batch["input_fields"]
+            x = batch["input_fields"]
+            x = x.to(device)
             x = rearrange(x, "B Ti Lx Ly F -> B (Ti F) Lx Ly")
             y = batch["output_fields"]
+            y = y.to(device)
             y = rearrange(y, "B To Lx Ly F -> B (To F) Lx Ly")
             yp = model(x)
             mse = (yp-y).square().mean()
             mse.backward()
             optimizer.zero_grad()
             bar.set_postfix(loss=mse.detach().item())
+        
             
     
 
