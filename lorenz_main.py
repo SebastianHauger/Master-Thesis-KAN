b30521a3d4eb1d974b6621f1411a78ffa88a204a
Sebastian Vogel Hauger <sebose2501@gmail.com>
2025-03-11 12:05:19 +0100

    updated gitignore

--- lorenz_main.py
+++ lorenz_main.py
@@ -2,5 +2,7 @@ import dafi
 
 
 if __name__=='__main__':
-    dafi.run('lorenz.py', "EnKF", nsamples=100, ntime=200, verbosity=1, inputs_model={"input_file": "lorenz.in"})
+    dafi.run('lorenz.py', "EnKF", nsamples=100, ntime=200,
+             save_level='time', save_dir='Results/results_dafi',
+             verbosity=1, inputs_model={"input_file": "lorenz.in"})
     
